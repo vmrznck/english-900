62880df23bb793fcf0ea711a979c7ef74ba9097f
vmrznck <vmrznck@gmail.com>
2026-01-12 23:21:05 +0800

    2026-01-12_v5

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "english900-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./English-900.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
